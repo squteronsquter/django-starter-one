5ca3eaa0b1480269ee902a87db42bb24b9ca304f
squteronsquter <squter@squter.pl>
2021-12-05 15:50:48 +0100

    four:dummy data dynamic with links

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# lets get some dummy data
+
+projectsList = [
+	{
+		'id': '1',
+		'title': 'Lorem Ipsum',
+		'description': 'Lorem ipsum dolor sit amet precurorum nihil sit'
+	},
+
+	{
+		'id': '2',
+		'title': 'Dolor Sit',
+		'description': 'Lorem ipsum dolor sit amet precurorum nihil sit'
+	},
+	{
+		'id': '3',
+		'title': 'Ament Nomet',
+		'description': 'Lorem ipsum dolor sit amet precurorum nihil sit'
+	},
+	{
+		'id': '4',
+		'title': 'Perpetuum Lorems',
+		'description': 'Lorem ipsum dolor sit amet precurorum nihil sit'
+	},
+]
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    msg = 'Hello, You are viewing the Projects App Home Page'
+    context = {'message':msg, 'projects':projectsList}
+    return render(request, 'projects/projects.html', context )
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project':projectObj})
 
