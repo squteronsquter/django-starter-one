07615723f7d881207c2ffdfc130c968227b82c62
squteronsquter <squter@squter.pl>
2021-12-05 12:47:34 +0100

    one:projects app routes

---
+++ projects/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse('This is our projects page')
+
+def project(request, pk):
+    return HttpResponse('Single project {}'.format(str(pk)))
+
